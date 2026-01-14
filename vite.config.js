ff428024ed3c8a5fb29224eceba9c23e29b417aa
Christopher Kilner <31750239+ClassicCK@users.noreply.github.com>
2026-01-14 14:26:28 +0000

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/isometric-pingpong/'
+  base: '/',  // Changed from '/isometric-pingpong/'
 })
